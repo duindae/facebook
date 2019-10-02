b2a400b7918339f962679502da31f634b4582d69
Bartosz Borczyk <bartosz.borczyk@softiq.pl>
2019-10-02 14:05:15 +0200

    menu oraz testy

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,8 +1,19 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {NotFoundPageComponent} from './pages/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
